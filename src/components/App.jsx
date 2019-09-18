9521169384d5f2b3e418428d3c8f0f40988dc6f7
Alex Siegberg <alex.siegberg@gmail.com>
2019-09-17 20:29:10 -0700

    Merge navbar components into single component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,27 @@
-import React, {Component} from 'react';
-import {Route, Switch} from 'react-router-dom';
-import '../App.css';
-import Container from 'react-bootstrap/Container';
-import UserNav from './Navbar/UserNav';
-import KegList from './KegList/KegList';
-import KegListEmployee from './KegListEmployee/KegListEmployee';
-import KegAdd from './KegAdd/KegAdd';
-import KegEdit from './KegEdit/KegEdit';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "../App.css";
+import Container from "react-bootstrap/Container";
+import UserNav from "./UserNav";
+import KegList from "./KegList/KegList";
+import KegListEmployee from "./KegListEmployee/KegListEmployee";
+import KegAdd from "./KegAdd/KegAdd";
+import KegEdit from "./KegEdit/KegEdit";
 
 function App() {
-
-    return (
-      <div className="App">
-        <UserNav/>
-        <Container>
-          <Switch>
-            <Route exact path='/' component={KegList} />
-            <Route exact path='/employee' component={KegListEmployee}/>
-            <Route exact path='/keg_add' component={KegAdd}/>
-            <Route exact path='/keg_edit' component={KegEdit}/>
-          </Switch>
-        </Container>
-      </div>
-    );
+  return (
+    <div className="App">
+      <UserNav />
+      <Container>
+        <Switch>
+          <Route exact path="/" component={KegList} />
+          <Route exact path="/employee" component={KegListEmployee} />
+          <Route exact path="/keg_add" component={KegAdd} />
+          <Route exact path="/keg_edit" component={KegEdit} />
+        </Switch>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
